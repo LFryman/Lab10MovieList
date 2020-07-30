1cd53336a46014df14ceba536d8e3edf6fe0cd81
lauren <lauren.a.fryman@gmail.com>
2020-07-30 19:55:38 -0400

    Initial commit

---
+++ MovieList10/Movie.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieList10
+{
+    class Movie
+    {
+        #region fields
+        private string _title;
+        private string _catagory;
+
+        #endregion
+
+        #region properties
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+        public string Catagory
+        {
+            get { return _catagory; }
+            set { _catagory = value; }
+        }
+
+        public Movie()
+        {
+
+        }
+
+        #endregion
+
+        #region Constructor 
+        public Movie(string title, string category)
+        {
+            _title = title;
+            _catagory = category;
+        }
+        #endregion 
+
+        public void Print()
+        {
+            Console.WriteLine(_title);
+        }
+    }
+}
---
+++ MovieList10/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieList10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> movies = new List<Movie>()
+            {
+            new Movie("Lord of The Rings", "fantacy"),
+            new Movie("The Matrix", "action"),
+            new Movie("Star Wars", "fantacy"),
+            new Movie("Princess Bride", "action"),
+            new Movie("Star Trek", "scifi"),
+            new Movie("Howells Moving Castle", "animation"),
+            new Movie("Spirited Away", "animation"),
+            new Movie("Star Gate", "scifi"),
+            new Movie("Serenity", "scifi"),
+            new Movie("Saving Private Ryan", "action")
+            };
+            bool lContinue = true;
+            while (lContinue)
+            {
+                Console.WriteLine("Welcome to the Movie List Application!");
+                Console.WriteLine("There are ten movies on this list to chose from.");
+                Console.WriteLine("Please enter your desired category.");
+                Console.WriteLine("1.) Fantacy.");
+                Console.WriteLine("2.) Action.");
+                Console.WriteLine("3.) Sci-Fi.");
+                Console.WriteLine("4.) Animation.");
+
+                try
+                {
+                    string userInput = Console.ReadLine();
+                    int userInput1 = int.Parse(userInput);
+
+                    if (userInput1 > 0 && userInput1 < 5)
+                    {
+                        if (userInput1 == 1)
+                        {
+                            foreach (Movie title in movies)
+                            {
+                                if (title.Catagory == "fantacy")
+                                {
+                                    title.Print();
+                                }
+                            }
+                        }
+                        else if (userInput1 == 2)
+                        {
+                            foreach (Movie title in movies)
+                            {
+                                if (title.Catagory == "action")
+                                {
+                                    title.Print();
+                                }
+                            }
+                        }
+                        else if (userInput1 == 3)
+                        {
+                            foreach (Movie title in movies)
+                            {
+                                if (title.Catagory == "scifi")
+                                {
+                                    title.Print();
+                                }
+                            }
+                        }
+                        else if (userInput1 == 4)
+                        {
+                            foreach (Movie title in movies)
+                            {
+                                if (title.Catagory == "animation")
+                                {
+                                    title.Print();
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("I highly suggest a number from 1 - 4. Try again.");
+                        Console.WriteLine("Press any key to try again");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("That input was not a number");
+                    Console.WriteLine("Press any key to try again");
+                    Console.ReadKey();
+                    Console.Clear();
+
+                }
+
+                Console.WriteLine("Would you like to pick another one? y/n");
+                string input2 = Console.ReadLine();
+                if (input2 == "y")
+                {
+                    lContinue = true;
+                }
+                else
+                {
+                    lContinue = false;
+                    Console.WriteLine("Come again!");
+                }
+            }
+        }
+    }
+}
+
+
